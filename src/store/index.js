2c1a78b5b89a73611ea23f69504c2d58c90555cd
Jason LeVan <jason@codeclarified.com>
2021-08-28 14:40:58 -0400

    Move character data to store

--- src/store/index.js
+++ src/store/index.js
@@ -14,6 +14,402 @@ import { createStore } from 'vuex'
 
 export default store(function (/* { ssrContext } */) {
   const Store = createStore({
+    state: {
+      characters: [
+        {
+          id: 1,
+          name: 'Lenny',
+          race: 'Troll',
+          owner: 'Jason',
+          status: [
+            'Fatigued'
+          ],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },{
+          id: 2,
+          name: 'Burt',
+          race: 'Elf',
+          owner: 'Joe',
+          status: [
+            'Burned',
+            'Immobile'
+          ],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },{
+          id: 3,
+          name: 'Sandy',
+          race: 'Human',
+          owner: 'Tim',
+          status: [],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },{
+          id: 4,
+          name: 'Thug1',
+          race: 'Orc',
+          owner: 'NPC',
+          status: [],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },{
+          id: 5,
+          name: 'Thug2',
+          race: 'Human',
+          owner: 'NPC',
+          status: [
+            'Dead'
+          ],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },{
+          id: 6,
+          name: 'Drone1',
+          race: 'Drone',
+          owner: 'Sandy',
+          status: [],
+          attributes: [
+            {
+              name: 'Body',
+              value: 5,
+              code: 'BOD'
+            },
+            {
+              name: 'Agility',
+              value: 4,
+              code: 'AGI'
+            },
+            {
+              name: 'Reaction',
+              value: 5,
+              code: 'REA'
+            },
+            {
+              name: 'Strength',
+              value: 2,
+              code: 'STR'
+            },
+            {
+              name: 'Charisma',
+              value: 5,
+              code: 'CHA'
+            },
+            {
+              name: 'Intuition',
+              value: 1,
+              code: 'INT'
+            },
+            {
+              name: 'Logic',
+              value: 2,
+              code: 'LOG'
+            },
+            {
+              name: 'Willpower',
+              value: 4,
+              code: 'WIL',
+            },
+            {
+              name: 'Edge',
+              value: 5,
+              code: 'EDG'
+            },
+            {
+              name: 'Magic',
+              value: 0,
+              code: 'MAG'
+            },
+            {
+              name: 'Resonance',
+              value: 0,
+              code: 'RES',
+            }
+          ]
+        },
+      ],
+    },
+    mutations: {
+
+    },
+    actions: {
+
+    },
     modules: {
       // example
     },
