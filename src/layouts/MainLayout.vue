82fc9c339027a9d54d13b0e145b64fd5c36f2494
Jason LeVan <jason@codeclarified.com>
2021-08-28 12:58:33 -0400

    Add test character data, update routes, update sidebar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,105 +10,73 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
+      <div class="q-px-lg q-pt-l q-mb-md">
+        <div class="text-h4">Shadow Helper</div>
+      </div>
     </q-header>
 
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
-      bordered
-      class="bg-grey-1"
+      :width="200"
+      :breakpoint="500"
     >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
+      <q-scroll-area class="fit">
+        <q-list padding class="menu-list">
+          <q-item
+            to="/"
+            exact
+            clickable
+            v-ripple>
+            <q-item-section avatar>
+              <q-icon name="house" />
+            </q-item-section>
 
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
+            <q-item-section>
+              Home
+            </q-item-section>
+          </q-item>
+
+          <q-item
+            to="/characters"
+            exact
+            clickable
+            v-ripple>
+            <q-item-section avatar>
+              <q-icon name="person" />
+            </q-item-section>
+
+            <q-item-section>
+              Characters
+            </q-item-section>
+          </q-item>
+
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
-
-const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
-];
-
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'MainLayout',
 
   components: {
-    EssentialLink
   },
 
   setup () {
     const leftDrawerOpen = ref(false)
 
     return {
-      essentialLinks: linksList,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
@@ -117,3 +85,7 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+
+</style>
---
+++ src/pages/Characters.vue
@@ -0,0 +1,78 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Characters</h5>
+
+    <q-item v-for="character in characters" :key="character.id">
+      <q-item-section top avatar>
+        <q-avatar rounded>
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+        </q-avatar>
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ character.name }}</q-item-label>
+        <q-item-label>Owner: {{ character.owner}} </q-item-label>
+        <q-item-label>Race: {{ character.race }} </q-item-label>
+      </q-item-section>
+
+      <q-item-section side top v-if="character.status.length">
+        <q-item-label v-for="(status, index) in character.status" :key="index">{{ status }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      characters: [
+        {
+          id: 1,
+          name: 'Lenny',
+          race: 'Troll',
+          owner: 'Jason',
+          status: [
+            'Fatigued'
+          ]
+        },{
+          id: 2,
+          name: 'Burt',
+          race: 'Elf',
+          owner: 'Joe',
+          status: [
+            'Burned',
+            'Immobile'
+          ]
+        },{
+          id: 3,
+          name: 'Sandy',
+          race: 'Human',
+          owner: 'Tim',
+          status: []
+        },{
+          id: 4,
+          name: 'Thug1',
+          race: 'Orc',
+          owner: 'NPC',
+          status: []
+        },{
+          id: 5,
+          name: 'Thug2',
+          race: 'Human',
+          owner: 'NPC',
+          status: [
+            'Dead'
+          ]
+        },{
+          id: 6,
+          name: 'Drone1',
+          race: 'Drone',
+          owner: 'Sandy',
+          status: []
+        },
+      ]
+    }
+  }
+}
+</script>
